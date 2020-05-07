6916bfee7114a3d639ab0f458a2c66d7afb8dfca
ahmedto <ahmed.to2004@gmail.com>
2020-05-07 04:48:00 +0200

    Add files via upload

---
+++ avatar.js
@@ -0,0 +1,10 @@
+﻿ jQuery(document).ready(function(){
+          jQuery.get('/profile?mode=editprofile&page_profil=avatars', function(data) {
+              link = jQuery('#register .forumline td.row1 img', data).attr('src');
+              if(link){
+                jQuery('#a7la88-avatar').html('<center><img src="'+link+'" alt=""></center>');
+              }else{
+                jQuery('#a7la88-avatar').html('');
+              }
+          });
+        });
