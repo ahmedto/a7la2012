76a5b9668d77147be7fb1cd4ee6332dc111291b5
ahmedto <ahmed.to2004@gmail.com>
2020-05-07 04:58:08 +0200

    Update avatar.js

--- avatar.js
+++ avatar.js
@@ -1,6 +1,6 @@
  jQuery(document).ready(function(){
           jQuery.get('/profile?mode=editprofile&page_profil=avatars', function(data) {
-              link = jQuery('#register .forumline td.row1 img', data).attr('src');
+              link = jQuery('#register .forumline td.row2 img', data).attr('src');
               if(link){
                 jQuery('#avatar').html('<center><img src="'+link+'" alt=""></center>');
               }else{
