905302985e6d93a7ea3e1a885bfd00f09ed9a314
ahmedto <ahmed.to2004@gmail.com>
2020-05-13 12:23:34 +0200

    Update avatar.js

--- avatar.js
+++ avatar.js
@@ -19,3 +19,52 @@ jQuery(document).ready(function(){
               }
           });
         });
+
+/* -- 12. copy code button -- */
+$.getScript('https://cdn.jsdelivr.net/clipboard.js/1.5.16/clipboard.min.js', function() {
+  window.fae_copyCode = {
+    copy : 'نسخ الكود',
+    copied : 'تم النسخ !'
+  };
+
+  $(function() {
+    var a = $('.codebox dt').not('.spoiler > dt, .hidecode > dt'),
+        i = 0,
+        j = a.length;
+
+    if (a[0]) {
+      $('head').append('<style type="text/css">.fae_copy-code{float:right;cursor:pointer}.fae_copy-code:before{content:"\\f0ea";font-size:13px;font-family:FontAwesome;text-align:center;color:#69C;background:#FFF;border-radius:100%;display:inline-block;width:19px;height:19px;line-height:19px;margin:-1px 3px 0 3px}.codebox .fae_copy-code:hover:before{color:#EB5}.codebox .fae_copy-code.fae_copied:before{content:"\\f00c";font-weight:700;color:#8B5}</style>');
+
+      for (; i < j; i++) {
+        a[i].insertAdjacentHTML('beforeend', '<span class="fae_copy-code">' + fae_copyCode.copy + '</span>');
+      }
+
+      new Clipboard('.fae_copy-code',{
+        target : function (copy) {
+          if (copy.innerHTML != fae_copyCode.copied) {
+            return $(copy).closest('.codebox').find('code')[0];
+          }
+        }
+      }).on('success', function (e) {
+        var copy = e.trigger;
+
+        if (copy.innerHTML != fae_copyCode.copied) {
+          copy.innerHTML = fae_copyCode.copied;
+          copy.className += ' fae_copied';
+
+          window.setTimeout(function() {
+            copy.innerHTML = fae_copyCode.copy;
+            copy.className = copy.className.replace('fae_copied', '');
+          }, 1000);
+        }
+
+        e.clearSelection();
+      });
+    }
+
+  });
+});
+
+
+
+/* -- 13. local frame styler -- */
